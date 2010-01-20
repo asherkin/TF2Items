a836f66b4ffac7f5e48cba77a21c4b482248cf3a
Asher Baker <asherkin@limetech.org>
2010-01-20 07:53:17 +0000

    Added the http://tf2items.limetech.org/ source files in ./website/

---
+++ website/colours.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Language" content="en-us" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>TF2Items Generator</title>
+<link rel="stylesheet" type="text/css" href="profile.css" />
+</head>
+
+<body style="background-color: #2A2725; color: #EBE2CA;">
+
+<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="js/itemhover.js"></script>
+
+<div class="pageContents">
+
+<a href=""><img src="images/logo.png" border="0" alt="Logo"/></a><br/>
+
+<div class="tf2rule"></div>
+
+<div id="backpackcontents" style="width: 986px; background-image: url('images/headers/colour_head.png'); background-repeat: no-repeat; padding: 80px 0 0 0;">
+
+<div class="invBox">
+<a class="item" href="attributes.php?class=<?php echo $_GET['class']; ?>&name=<?php echo $_GET['name']; ?>&index=<?php echo $_GET['index']; ?>&qualityname=QualityColorrarity1&quality=1"><img class="item" src="images/qualities/QualityColorrarity1.png" /></a>
+</div>
+
+<div class="invBox">
+<a class="item" href="attributes.php?class=<?php echo $_GET['class']; ?>&name=<?php echo $_GET['name']; ?>&index=<?php echo $_GET['index']; ?>&qualityname=QualityColorrarity2&quality=2"><img class="item" src="images/qualities/QualityColorrarity2.png" /></a>
+</div>
+
+<div class="invBox">
+<a class="item" href="attributes.php?class=<?php echo $_GET['class']; ?>&name=<?php echo $_GET['name']; ?>&index=<?php echo $_GET['index']; ?>&qualityname=QualityColorUnique&quality=3"><img class="item" src="images/qualities/QualityColorUnique.png" /></a>
+</div>
+
+<div class="invBox">
+<a class="item" href="attributes.php?class=<?php echo $_GET['class']; ?>&name=<?php echo $_GET['name']; ?>&index=<?php echo $_GET['index']; ?>&qualityname=QualityColorrarity3&quality=4"><img class="item" src="images/qualities/QualityColorrarity3.png" /></a>
+</div>
+
+<div class="invBox">
+<a class="item" href="attributes.php?class=<?php echo $_GET['class']; ?>&name=<?php echo $_GET['name']; ?>&index=<?php echo $_GET['index']; ?>&qualityname=QualityColorrarity4&quality=5"><img class="item" src="images/qualities/QualityColorrarity4.png" /></a>
+</div>
+
+<div class="invBox">
+<a class="item" href="attributes.php?class=<?php echo $_GET['class']; ?>&name=<?php echo $_GET['name']; ?>&index=<?php echo $_GET['index']; ?>&qualityname=QualityColorrarity5&quality=6"><img class="item" src="images/qualities/QualityColorrarity5.png" /></a>
+</div>
+
+<div class="invBox">
+<a class="item" href="attributes.php?class=<?php echo $_GET['class']; ?>&name=<?php echo $_GET['name']; ?>&index=<?php echo $_GET['index']; ?>&qualityname=QualityColorNormal&quality=9"><img class="item" src="images/qualities/QualityColorNormal.png" /></a>
+</div>
+
+<div class="invBox">
+</div>
+
+<div class="invBox">
+</div>
+
+<div class="invBox">
+</div>
+
+<div style="clear: both"></div> 
+</div>
+
+</div>
+</body>
+</html>
