33b0bcd6ad080a84e7595c1058de71ef3b64738a
Asher Baker <asherkin@limetech.org>
2013-07-20 00:03:25 +0100

    Removed upper bound on TF2Items_SetQuality.

--- extension/extension.cpp
+++ extension/extension.cpp
@@ -702,7 +702,7 @@ static cell_t TF2Items_SetQuality(IPluginContext *pContext, const cell_t *params
 	TScriptedItemOverride * pScriptedItemOverride = GetScriptedItemOverrideFromHandle(params[1], pContext);
 	if (pScriptedItemOverride != NULL)
 	{
-		if (params[2] < -1 || params[2] > 14) return pContext->ThrowNativeError("Quality out of bounds: %i [-1 ... 14]", params[2]);
+		if (params[2] < -1) return pContext->ThrowNativeError("Quality out of bounds: %i [-1 ...]", params[2]);
 		pScriptedItemOverride->m_iEntityQuality = params[2];
 	}
 	return 0;
